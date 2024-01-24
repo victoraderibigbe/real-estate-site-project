0c7e968f5394c8d8e53e9eac6fa22eefff157448
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2024-01-24 17:18:34 +0100

    Adjust sizings for small screens

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,7 +2,7 @@ import { FaSearch } from "react-icons/fa";
 
 const Searchbar = () => {
   return (
-    <div className="my-4 w-full text-indigo-800 rounded-xl bg-white flex items-center p-1 md:p-2 pr-5">
+    <div className="my-4 w-full text-indigo-800 rounded-xl bg-white flex items-center p-1 pr-5 md:p-2 md:pr-10">
       <input
         className="w-full h-10 md:h-16 mr-5 text-lg md:text-xl border-none focus-within:ring-0"
         type="text"
