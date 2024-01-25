dbfe4604ee3e09171658c80c070fc980cc761651
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2024-01-25 16:43:49 +0100

    Update navbar behaviour on scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,37 @@ import siteLogo from "../assets/img/dominion-homes-logo.png";
 import MyButton from "./MyButton";
 import { SlCallOut } from "react-icons/sl";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0); // State to track the previous scroll position
+  const [visible, setVisible] = useState(true); // State to track the visibility of the navbar
+
+  // Effect hook to handle scroll events
+  useEffect(() => {
+    // Event handler for the scroll event
+    const handleScroll = () => {
+      // Get the current scroll position in the vertical axis
+      const currentScrollPos = window.scrollY;
+
+      // Determine whether to make the navbar visible or not
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+
+      // Update the previous scroll position with the current one
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
+
   return (
-    <div className="sticky top-0 z-50">
+    <div className={visible ? "navbar-visible" : "navbar-hidden"}>
       <Navbar fluid rounded>
         <Navbar.Brand>
           <Link to="/">
@@ -18,7 +45,7 @@ const Header = () => {
           </Link>
         </Navbar.Brand>
         <div className="flex md:order-2">
-          <a href="tel:2349013284694">
+          <a href="tel:+2349013284694">
             <MyButton title={"Call Us Now"} icon={<SlCallOut />} />
           </a>
           <Navbar.Toggle />
